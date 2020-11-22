cc3ad97d0ddc6c7b8d57265c8ceb32fa74715e15
Jasmine Lu <jasminelu@uchicago.edu>
2020-11-22 12:12:18 -0600

    touch interaction with right hand and arm ui
    added physics colliders to enable touch interaction from right hand

---
+++ Assets/Scripts/ButtonTrigger.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(Collider))]
+
+public class ButtonTrigger : MonoBehaviour
+{
+    [SerializeField]
+    private UnityEvent onButtonPressed;
+
+    public bool pressedInProgress = false;
+    // Start is called before the first frame update
+    private void OnTriggerEnter(Collider other)
+    {
+        if(!pressedInProgress)
+        {
+            pressedInProgress = true;
+            onButtonPressed?.Invoke();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+            pressedInProgress = false;
+            onButtonPressed?.Invoke();
+    }
+}
